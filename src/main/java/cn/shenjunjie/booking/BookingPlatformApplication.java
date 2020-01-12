f5c568c754ac7700df2fd71912831030675f3763
shenjunjie <junjie.shen@oriente.com>
2020-01-12 14:30:56 +0800

    feat: init git repository

---
+++ src/main/java/cn/shenjunjie/booking/BookingPlatformApplication.java
@@ -0,0 +1,13 @@
+package cn.shenjunjie.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/shenjunjie/booking/BookingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.shenjunjie.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
